76f67d6e39ae757c79056afdbd1d91d25ac0b06f
Martin Kovalski <kovm24@vse.cz>
2023-11-23 00:16:26 +0100

    new dtb scheme

--- migrations/structures/2023-11-22-154625-init.sql
+++ migrations/structures/2023-11-22-154625-init.sql
@@ -1,3 +1,16 @@
+create table categories
+(
+    category_id int auto_increment
+        primary key,
+    name        varchar(255)      not null,
+    parent_id   int               null,
+    showed      tinyint default 0 not null,
+    constraint categories_categories_category_id_fk
+        foreign key (parent_id) references categories (category_id)
+            on update cascade
+)
+    collate = utf8mb4_czech_ci;
+
 create table products
 (
     product_id  int auto_increment
@@ -7,7 +20,24 @@ create table products
     created     timestamp default current_timestamp() not null,
     updated     timestamp                             null on update current_timestamp(),
     stock       int       default 0                   not null,
-    category_id int                                   not null
+    category_id int                                   not null,
+    showed      tinyint   default 0                   not null,
+    deleted     tinyint   default 0                   not null,
+    constraint products_categories_category_id_fk
+        foreign key (category_id) references categories (category_id)
+            on update cascade
+)
+    collate = utf8mb4_czech_ci;
+
+create table product_photos
+(
+    product_photo_id int auto_increment
+        primary key,
+    path             varchar(255) not null,
+    product_id       int          not null,
+    constraint product_photos_products_product_id_fk
+        foreign key (product_id) references products (product_id)
+            on update cascade
 )
     collate = utf8mb4_czech_ci;
 
@@ -36,9 +66,11 @@ create table permissions
     action        varchar(255)                           not null,
     type          enum ('allow', 'deny') default 'allow' not null,
     constraint permissions_resources_resource_id_fk
-        foreign key (resource_id) references eshop.resources (resource_id),
+        foreign key (resource_id) references resources (resource_id)
+            on update cascade on delete cascade,
     constraint permissions_roles_role_id_fk
-        foreign key (role_id) references eshop.roles (role_id)
+        foreign key (role_id) references roles (role_id)
+            on update cascade on delete cascade
 )
     collate = utf8mb4_czech_ci;
 
@@ -51,8 +83,11 @@ create table users
     facebook_id varchar(255)                null,
     role_id     int                         not null,
     password    varchar(255)                null,
+    blocked     tinyint default 0           not null,
+    deleted     tinyint default 0           not null,
     constraint users_roles_role_id_fk
-        foreign key (role_id) references eshop.roles (role_id)
+        foreign key (role_id) references roles (role_id)
+            on update cascade
 )
     collate = utf8mb4_czech_ci;
 
@@ -65,7 +100,51 @@ create table addresses
     zip        varchar(255) not null,
     user_id    int          not null,
     constraint addresses_users_user_id_fk
-        foreign key (user_id) references eshop.users (user_id)
+        foreign key (user_id) references users (user_id)
+            on update cascade
+)
+    collate = utf8mb4_czech_ci;
+
+create table carts
+(
+    cart_id       int auto_increment
+        primary key,
+    user_id       int                                   not null,
+    last_modified timestamp default current_timestamp() not null,
+    constraint carts_users_user_id_fk
+        foreign key (user_id) references users (user_id)
+            on update cascade
+)
+    collate = utf8mb4_czech_ci;
+
+create table cart_items
+(
+    cart_item_id int auto_increment
+        primary key,
+    cart_id      int           not null,
+    product_id   int           not null,
+    quantity     int default 1 not null,
+    constraint cart_items_carts_cart_id_fk
+        foreign key (cart_id) references carts (cart_id)
+            on update cascade on delete cascade,
+    constraint cart_items_products_product_id_fk
+        foreign key (product_id) references products (product_id)
+            on update cascade
+)
+    collate = utf8mb4_czech_ci;
+
+create table favourite_products
+(
+    favourite_product_id int auto_increment
+        primary key,
+    product_id           int not null,
+    user_id              int not null,
+    constraint favourite_products_products_product_id_fk
+        foreign key (product_id) references products (product_id)
+            on update cascade,
+    constraint favourite_products_users_user_id_fk
+        foreign key (user_id) references users (user_id)
+            on update cascade
 )
     collate = utf8mb4_czech_ci;
 
@@ -77,7 +156,8 @@ create table forgotten_passwords
     code                  varchar(255)                          not null,
     created               timestamp default current_timestamp() not null,
     constraint forgotten_passwords_users_user_id_fk
-        foreign key (user_id) references eshop.users (user_id)
+        foreign key (user_id) references users (user_id)
+            on update cascade
 )
     collate = utf8mb4_czech_ci;
 
@@ -85,16 +165,52 @@ create table orders
 (
     order_id  int auto_increment
         primary key,
-    adress_id int                                                                               not null,
-    status    enum ('received', 'processing', 'ready', 'delivered') default 'received'          not null,
-    created   timestamp                                             default current_timestamp() not null,
-    user_id   int                                                                               not null,
-    shipping  enum ('vyzvednuti', 'zasilkovna', 'posta', 'ppl')                                 not null,
-    payment   enum ('cash', 'card', 'bank_transfer')                                            not null,
+    adress_id int                                                                                            not null,
+    status    enum ('received', 'processing', 'ready', 'delivered', 'cancelled') default 'received'          not null,
+    created   timestamp                                                          default current_timestamp() not null,
+    user_id   int                                                                                            not null,
+    shipping  enum ('vyzvednuti', 'zasilkovna', 'posta', 'ppl')                                              not null,
+    payment   enum ('cash', 'card', 'bank_transfer')                                                         not null,
     constraint orders_addresses_address_id_fk
-        foreign key (adress_id) references eshop.addresses (address_id),
+        foreign key (adress_id) references addresses (address_id)
+            on update cascade,
     constraint orders_users_user_id_fk
-        foreign key (user_id) references eshop.users (user_id)
+        foreign key (user_id) references users (user_id)
+            on update cascade
+)
+    collate = utf8mb4_czech_ci;
+
+create table order_items
+(
+    order_item_id int auto_increment
+        primary key,
+    quantity      int default 1 not null,
+    product_id    int           not null,
+    order_id      int           not null,
+    constraint order_items_orders_order_id_fk
+        foreign key (order_id) references orders (order_id),
+    constraint order_items_products_product_id_fk
+        foreign key (product_id) references products (product_id)
+            on update cascade
+)
+    collate = utf8mb4_czech_ci;
+
+create table reviews
+(
+    review_id  int auto_increment
+        primary key,
+    text       text null,
+    stars      int  not null,
+    user_id    int  null,
+    product_id int  not null,
+    constraint reviews_products_product_id_fk
+        foreign key (product_id) references products (product_id)
+            on update cascade,
+    constraint reviews_users_user_id_fk
+        foreign key (user_id) references users (user_id)
+            on update cascade on delete set null,
+    constraint check_stars
+        check (`stars` >= 1 and `stars` <= 5)
 )
     collate = utf8mb4_czech_ci;
 
---
+++ migrations/structures/2023-11-22-221023-order-typo.sql
@@ -0,0 +1,2 @@
+alter table orders
+    change adress_id address_id int not null;
