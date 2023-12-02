6ed0952c8a89f1a0abb50335b33fe7671192f53c
jirka <andrlik.jirka@gmail.com>
2023-12-02 23:50:52 +0100

    add migrations for inserting dummy categories and dummy products

---
+++ migrations/dummy-data/2023-12-02-234625-dummy-categories.sql
@@ -0,0 +1,7 @@
+INSERT INTO eshop.categories (category_id, name, parent_id, showed) VALUES (1, 'Kategorie 1', null, 1);
+INSERT INTO eshop.categories (category_id, name, parent_id, showed) VALUES (2, 'Kategorie 2', null, 1);
+INSERT INTO eshop.categories (category_id, name, parent_id, showed) VALUES (3, 'Podkategorie 1.1', 1, 0);
+INSERT INTO eshop.categories (category_id, name, parent_id, showed) VALUES (4, 'Podkategorie 1.2', 1, 0);
+INSERT INTO eshop.categories (category_id, name, parent_id, showed) VALUES (5, 'Podkategorie 2.1', 2, 0);
+INSERT INTO eshop.categories (category_id, name, parent_id, showed) VALUES (6, 'Podkategorie 2.2', 2, 0);
+INSERT INTO eshop.categories (category_id, name, parent_id, showed) VALUES (7, 'Podpodkategorie 1.1.1', 3, 0);
