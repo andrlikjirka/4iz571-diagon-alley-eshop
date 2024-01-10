0448a7efd9087bdf77c2062f8441c64111ab63ac
andrlikjirka <andrlik.jirka@gmail.com>
2024-01-10 11:12:28 +0100

    stock edit (not working)

--- app/AdminModule/DataGrids/ProductsDataGrid/ProductsDataGridControl.php
+++ app/AdminModule/DataGrids/ProductsDataGrid/ProductsDataGridControl.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace App\AdminModule\DataGrids\ProductsDataGrid;
 
 
+use App\Forms\FormFactory;
 use App\Model\Facades\ProductsFacade;
 use App\Model\Orm\Products\Product;
 use Nette\Application\AbortException;
 use Nette\Application\UI\Control;
 use Nette\Bridges\ApplicationLatte\Template;
+use Nette\Forms\Form;
+use Nette\Tokenizer\Exception;
 use stdClass;
 use Ublaboo\DataGrid\Column\Action\Confirmation\CallbackConfirmation;
 use Ublaboo\DataGrid\DataGrid;
@@ -23,70 +26,107 @@ use Ublaboo\DataGrid\DataGrid;
  */
 class ProductsDataGridControl extends Control
 {
-	public function __construct(
-		private readonly ProductsFacade $productsFacade
-	) {}
+    public function __construct(
+        private readonly ProductsFacade $productsFacade,
+        private readonly FormFactory $formFactory
+    )
+    {
+    }
 
-	public function createComponentDataGrid(): DataGrid
-	{
-		$grid = new DataGrid();
+    public function createComponentDataGrid(): DataGrid
+    {
+        $grid = new DataGrid();
 
-		$grid->setTemplateFile(__DIR__ . '/templates/datagridTemplate.latte');
+        $grid->setTemplateFile(__DIR__ . '/templates/datagridTemplate.latte');
 
-		$grid->setDataSource($this->productsFacade->findAllProducts());
+        $grid->setDataSource($this->productsFacade->findAllProducts());
 
-		$grid->addColumnNumber('id', 'ID produktu')
-			->setAlign('left')
-			->setSortable();
+        $grid->addColumnNumber('id', 'ID produktu')
+            ->setAlign('left')
+            ->setSortable();
 
-		$grid->addColumnText('name', 'Název produktu')
-			->setSortable()
-			->setAlign('left')
-			->setFilterText();
+        $grid->addColumnText('name', 'Název produktu')
+            ->setSortable()
+            ->setAlign('left')
+            ->setFilterText();
 
-		$grid->addColumnNumber('stock', 'Skladem')
-			->setSortable()
-			->setAlign('left')
-			->setRenderer(function (Product $product): string {
-				return $product->stock . " ks";
-			});
+        $grid->addColumnNumber('stock', 'Skladem')
+            ->setSortable()
+            ->setAlign('left')
+            ->setRenderer(function (Product $product): string {
+                return $product->stock . " ks";
+            });
 
-		$grid->addColumnNumber('price', 'Cena produktu')
-			->setAlign('left')
-			->setRenderer(function (Product $product): string {
-				return $product->galleonPrice . "G " . $product->sicklePrice . "S " . $product->knutPrice . "K";
-			});
+        $grid->addColumnNumber('price', 'Cena produktu')
+            ->setAlign('left')
+            ->setRenderer(function (Product $product): string {
+                return $product->galleonPrice . "G " . $product->sicklePrice . "S " . $product->knutPrice . "K";
+            });
 
-		$grid->addAction('showed', '')
-			->setIcon(function (Product $product): string {
-				return $product->showed ? 'eye' : 'eye-slash';
-			})
-			->setClass('ajax btn btn-xs btn-secondary ms-1 me-1 mb-1');
+        $grid->addAction('showed', '')
+            ->setIcon(function (Product $product): string {
+                return $product->showed ? 'eye' : 'eye-slash';
+            })
+            ->setClass('ajax btn btn-xs btn-secondary ms-1 me-1 mb-1');
 
 
-		$grid->addActionCallback('edit', '')
-			->setIcon('pen-to-square')
-			->setClass('btn btn-xs btn-warning ms-1 me-1 mb-1')
-			->onClick[] = function ($itemId): void {
-				$this->presenter->redirect('Products:edit', ['id' => $itemId]);
-		};
+        $grid->addActionCallback('edit', '')
+            ->setIcon('pen-to-square')
+            ->setClass('btn btn-xs btn-warning ms-1 me-1 mb-1')
+            ->onClick[] = function ($itemId): void {
+            $this->presenter->redirect('Products:edit', ['id' => $itemId]);
+        };
 
-		$grid->addAction('delete', '')
-			->setIcon('trash')
-			->setClass('ajax btn btn-xs btn-danger ms-1 me-1 mb-1')
+        $grid->addAction('delete', '')
+            ->setIcon('trash')
+            ->setClass('ajax btn btn-xs btn-danger ms-1 me-1 mb-1')
             ->setConfirmation(
                 new CallbackConfirmation(
-                    function($product) {
-                        return 'Opravdu chcete odstranit produkt '.$product->name.'?';
+                    function ($product) {
+                        return 'Opravdu chcete odstranit produkt ' . $product->name . '?';
                     }
                 )
             );
 
-		$grid->setItemsPerPageList([1, 10, 20, 50, 100, 200, 500], false)
+        $grid->addColumnText('stockEdit', '');
+
+        $grid->addAction('saveStock', 'uložit ks')
+            ->setClass('ajax btn btn-xs btn-light');
+
+        $grid->setItemsPerPageList([1, 10, 20, 50, 100, 200, 500], false)
             ->setDefaultPerPage(10);
 
-		return $grid;
-	}
+        return $grid;
+    }
+
+    public function handleSaveStock(): void
+    {
+        $stock = $this->presenter->getParameter('stock');
+        $productId = intval($this->presenter->getParameter('productId'));
+        bdump($stock);
+        bdump($productId);
+
+        try {
+            $product = $this->productsFacade->getProduct($productId);
+        } catch (Exception $e) {
+            $this->presenter->flashMessage('Požadovaný produkt nebyl nalezen.', 'danger');
+            $this->redirect('this');
+        }
+        $product->stock = $stock;
+        try {
+            $this->productsFacade->saveProduct($product);
+        } catch (\Exception $e) {
+            $this->presenter->flashMessage('Skladové zásoby produktu '.$product->name.' se nepodařilo změnit.', 'danger');
+        }
+        $this->presenter->flashMessage('Skladové zásoby produktu '.$product->name.' byly úspěšně změněny na '.$stock.' ks.', 'success');
+
+        if($this->presenter->isAjax()) {
+            $this->presenter->redrawControl('flashes');
+            $this['dataGrid']->redrawItem($product->id);
+        } else {
+            $this->presenter->redirect('this');
+        }
+    }
 
 	/**
 	 * @throws AbortException
