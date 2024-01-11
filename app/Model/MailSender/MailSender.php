dfd76f567aad0f152a7b3c0710841bf84301a39f
andrlikjirka <andrlik.jirka@gmail.com>
2024-01-11 01:01:27 +0100

    mailsender parameters in config

--- app/Model/MailSender/MailSender.php
+++ app/Model/MailSender/MailSender.php
@@ -4,24 +4,27 @@ namespace App\Model\MailSender;
 
 use App\Model\Orm\Orders\Order;
 use App\Model\Orm\Users\User;
+use App\Settings;
 use Latte\Engine;
 use Nette\Mail\Mailer;
 use Nette\Mail\Message;
 
 class MailSender
 {
-    //TODO: tohle by bylo fajn mít jako konstanty v configu
-    private string $mailFromEmail = 'andj10@vse.cz';
-    private string $mailFromName = 'Diagon Alley eshop';
+    private string $mailFrom;
+    private string $nameFrom;
 
     public function __construct(
         private readonly Mailer $mailer,
-        private readonly Engine $latte
+        private readonly Engine $latte,
+        private readonly Settings $settings
     )
     {
+        $this->mailFrom = $this->settings->mail['mailFrom'];
+        $this->nameFrom = $this->settings->mail['nameFrom'];
     }
 
-    public function sendForgottenPasswordMail(User $user, string $mailLink)
+    public function sendForgottenPasswordMail(User $user, string $mailLink): void
     {
         $params = [
             'user' => $user,
@@ -29,7 +32,7 @@ class MailSender
         ];
 
         $mail = new Message();
-        $mail->setFrom($this->mailFromEmail, $this->mailFromName);
+        $mail->setFrom($this->mailFrom, $this->nameFrom);
         $mail->addTo($user->email, $user->name);
         $mail->subject = 'Obnova zapomenutého hesla';
         //$mail->htmlBody = 'Obdrželi jsme vaši žádost na obnovu zapomenutého hesla. Pokud si přejete heslo změnit, <a href="' . $mailLink . '">klikněte zde</a>.';
@@ -40,7 +43,7 @@ class MailSender
         $this->mailer->send($mail); //případně smtpMailer
     }
 
-    public function sendNewUserMail(User $user, string $mailLink)
+    public function sendNewUserMail(User $user, string $mailLink): void
     {
         $params = [
           'user' => $user,
@@ -48,7 +51,7 @@ class MailSender
         ];
 
         $mail = new Message();
-        $mail->setFrom($this->mailFromEmail, $this->mailFromName);
+        $mail->setFrom($this->mailFrom, $this->nameFrom);
         $mail->addTo($user->email, $user->name);
         $mail->subject = 'Nový uživatel eshopu z Příčné ulice';
         //$mail->htmlBody = 'Byl jste přidán jako nový uživatel. Pro nastavení nového hesla klikněte zde:, <a href="' . $mailLink . '">klikněte zde</a>.';
@@ -59,7 +62,7 @@ class MailSender
         $this->mailer->send($mail); // případně smtpMailer
     }
 
-    public function sendNewOrderMail(Order $order, User $user)
+    public function sendNewOrderMail(Order $order, User $user): void
     {
         $params = [
             'order' => $order,
@@ -67,7 +70,7 @@ class MailSender
         ];
 
         $mail = new Message();
-        $mail->setFrom($this->mailFromEmail, $this->mailFromName);
+        $mail->setFrom($this->mailFrom, $this->nameFrom);
         $mail->addTo($user->email, $user->name);
         $mail->subject = 'Potvrzení objednávky č. '.$order->id;
         //$mail->htmlBody = 'Byl jste přidán jako nový uživatel. Pro nastavení nového hesla klikněte zde:, <a href="' . $mailLink . '">klikněte zde</a>.';
