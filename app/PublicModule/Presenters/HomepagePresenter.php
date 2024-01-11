2c4e3055734a60d1fbbac65d7d88f0c0f07030ef
Martin Kovalski <kovm24@vse.cz>
2024-01-12 00:12:45 +0100

    sitemap generating

--- app/PublicModule/Presenters/HomepagePresenter.php
+++ app/PublicModule/Presenters/HomepagePresenter.php
@@ -4,7 +4,27 @@ declare(strict_types=1);
 
 namespace App\PublicModule\Presenters;
 
+use App\Model\Facades\CategoriesFacade;
+use App\Model\Facades\ProductsFacade;
 
+
+/**
+ * Class HomepagePresenter
+ * @package App\PublicModule\Presenters
+ * @author Martin Kovalski
+ */
 final class HomepagePresenter extends BasePresenter
 {
+	public function __construct(
+		private readonly ProductsFacade $productsFacade,
+		private readonly CategoriesFacade $categoriesFacade
+	) {
+		parent::__construct();
+	}
+
+	public function renderSitemap(): void
+	{
+		$this->template->categories = $this->categoriesFacade->getAllCategories();
+		$this->template->products = $this->productsFacade->getAllProducts();
+	}
 }
