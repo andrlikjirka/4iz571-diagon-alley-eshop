5c5dee58997db7b42ade7334bf2ab278d64fc7ca
andrlikjirka <andrlik.jirka@gmail.com>
2024-01-12 02:09:23 +0100

    migrations categories

---
+++ migrations/basic-data/2024-01-11-230056-categories-products.sql
@@ -0,0 +1,8 @@
+INSERT INTO eshop.categories (name, slug, parent_id, showed, deleted) VALUES ('Létající košťata', 'letajici-kostata', null, 1, 0);
+INSERT INTO eshop.categories (name, slug, parent_id, showed, deleted) VALUES ('Kouzelnické hůlky', 'kouzelnicke-hulky', null, 1, 0);
+INSERT INTO eshop.categories (name, slug, parent_id, showed, deleted) VALUES ('Oblečení', 'obleceni', null, 1, 0);
+INSERT INTO eshop.categories (name, slug, parent_id, showed, deleted) VALUES ('Čarodějnické knihy', 'carodejnicke-knihy', null, 1, 0);
+INSERT INTO eshop.categories (name, slug, parent_id, showed, deleted) VALUES ('Nimbus Racing Broom Company', 'nimbus-racing-broom-company', 12, 1, 0);
+INSERT INTO eshop.categories (name, slug, parent_id, showed, deleted) VALUES ('Cleansweep Broom Company', 'cleansweep-broom-company', 12, 1, 0);
+INSERT INTO eshop.categories (name, slug, parent_id, showed, deleted) VALUES ('Comet Trading Company', 'comet-trading-company', 12, 0, 0);
+INSERT INTO eshop.categories (name, slug, parent_id, showed, deleted) VALUES ('Kolejní hábity', 'kolejni-habity', 14, 1, 0);
