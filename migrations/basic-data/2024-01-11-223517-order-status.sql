7a13c0a15767e4c6826f21e404251b9e34832d6b
andrlikjirka <andrlik.jirka@gmail.com>
2024-01-12 01:45:51 +0100

    migrations orderstatus data

---
+++ migrations/basic-data/2024-01-11-223517-order-status.sql
@@ -0,0 +1,4 @@
+INSERT INTO eshop.order_status (order_status_id, name) VALUES (1, 'Přijatá');
+INSERT INTO eshop.order_status (order_status_id, name) VALUES (2, 'Zpracovává se');
+INSERT INTO eshop.order_status (order_status_id, name) VALUES (3, 'Vyřízená');
+INSERT INTO eshop.order_status (order_status_id, name) VALUES (4, 'Stornovaná');
